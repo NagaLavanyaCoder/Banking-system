73f18e5a4f2423329f82827c579cf4a9dcae3970
Naga Lavanya <metlanagalavanya7@gmail.com>
2026-07-07 16:02:03 +0530

    Initial Spring Boot Project Setup

---
+++ banking-transaction-system/src/test/java/com/lavanya/banking_system/BankingTransactionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavanya.banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
